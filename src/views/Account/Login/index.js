23eca2bd81f4ebe464ffe0a1a80211caa8ea79fc
wanjun <243958407@qq.com>
2018-07-06 18:32:49 +0800

    配置icon, https://blog.csdn.net/jiecsdn/article/details/60867232

--- src/views/Account/Login/index.js
+++ src/views/Account/Login/index.js
@@ -1,35 +1,115 @@
 import React, { Component } from 'react';
 import {
   StyleSheet,
+  Dimensions,
+} from 'react-native';
+// import {
+//   Actions,
+// } from 'react-native-router-flux';
+import {
+  Button,
   Text,
   View,
-  Button,
-} from 'react-native';
+} from 'antd-mobile-rn';
 import {
-  Actions,
-} from 'react-native-router-flux';
+  Icon,
+} from 'react-native-vector-icons';
+import Resolution from '../../../components/FontSize';
 
+const { height, width } = Dimensions.get('window');
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    height,
+    width,
+    fontSize: 16,
+  },
+  fontSize16: {
+    fontSize: 16,
+  },
+  navbar: {
+    height: 40,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    backgroundColor: '#30bf6c',
   },
 });
 
-export default class Test3 extends Component {
+export default class Login extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      user: '',
+    };
   }
 
+  componentDidMount() {
+    storage.save({
+      key: 'loginState',
+      data: {
+        from: 'some other site',
+        userid: 'some userid',
+        token: 'some token',
+      },
+      expires: 1000 * 3600,
+    });
+
+    // 读取
+    storage.load({
+      key: 'loginState',
+      autoSync: true,
+      syncInBackground: true,
+      syncParams: {
+        extraFetchOptions: {
+          // 各种参数
+        },
+        someFlag: true,
+      },
+    }).then((ret) => {
+      console.log(ret.userid);
+      this.setState({ user: ret });
+      const {
+        user,
+      } = this.state;
+      console.log(user);
+    }).catch((err) => {
+      console.warn(err.message);
+    });
+  }
+
+  // https://blog.csdn.net/jiecsdn/article/details/60867232
   render() {
     return (
-      <View style={styles.container}>
-        <Text>
-          登陆
-        </Text>
-        <Button title="go to student" onPress={() => Actions.student()} />
-        <Button title="go to teacher" onPress={() => Actions.teacher()} />
-      </View>
+      <Resolution.FixWidthView>
+        <View style={styles.navbar}>
+          <Button>
+            <Icon name="rocket" size={16} color="red" />
+          </Button>
+          <Text>
+            12-23数学作业
+          </Text>
+          <Text>
+            计时00:00 /预估30分钟
+          </Text>
+        </View>
+        {/* <View style={styles.container}>
+          <Text>
+            登陆
+          </Text>
+          <Button onClick={() => Actions.student()}>
+            go to student
+          </Button>
+          <Button onClick={() => Actions.teacher()}>
+            go to teacher
+          </Button>
+          <Button>
+            设置storage
+          </Button>
+          <Button>
+            获取storage
+          </Button>
+        </View> */}
+      </Resolution.FixWidthView>
     );
   }
 }
