5d8d6ba428ee0239134deb9278ca52f1dc7d6293
wanjun <243958407@qq.com>
2018-08-03 20:04:49 +0800

    fix：图标使用demo

--- Example/index.js
+++ Example/index.js
@@ -3,10 +3,12 @@ import {
   View,
 } from 'react-native';
 import SvgUri from '../src/components/Svg';
+import CIcon from '../src/components/Icon';
 
 const Example = () => (
   <View>
     <SvgUri height="20" width="20" source="examBook" />
+    <CIcon style={{ fontSize: 20, color: '#000' }} name="suo" size={25} />
   </View>
 );
 
